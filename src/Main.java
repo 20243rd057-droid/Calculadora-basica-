60a5c0e8097a8a51818369a21fac1a1146baa698
20243rd057-droid <20243rd057@utez.edu.mx>
2026-05-12 20:39:19 -0600

    herror de ejecucion

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,20 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Ingresa el primer número: ");
+        System.out.print("ingresa el primer numero: ");
         double num1 = sc.nextDouble();
 
-        System.out.print("Ingresa el segundo número: ");
+        System.out.print("ingresa el segundo numero: ");
         double num2 = sc.nextDouble();
 
-        System.out.println("Suma: " + trabajo.sumar(num1, num2));
+        System.out.println("suma: " + trabajo.sumar(num1, num2));
 
-        System.out.println("Resta: " + trabajo.restar(num1, num2));
+        System.out.println("resta: " + trabajo.restar(num1, num2));
 
-        System.out.println("Multiplicación: " + trabajo.multiplicar(num1, num2));
+        System.out.println("multiplicación: " + trabajo.multiplicar(num1, num2));
 
         if (num2 != 0) {
-            System.out.println("División: " + trabajo.dividir(num1, num2));
+            System.out.println("division: " + trabajo.dividir(num1, num2));
         } else {
             System.out.println("No se puede dividir entre cero.");
         }
